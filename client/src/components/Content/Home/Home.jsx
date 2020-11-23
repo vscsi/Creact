a05c0ac6ed535db48cc05d97e44618fda2f62a48
Venus Chan <vchan446@gmail.com>
2020-11-23 18:12:00 +0800

    first commit

--- client/src/components/Content/Home/Home.jsx
+++ client/src/components/Content/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import HeroSection from "./HeroSection";
+import Aux from "../../../hoc/Auxiliary"
 
 const Home = () => {
   return (
-    <>
+    <Aux>
       <Header />
       <HeroSection />
-    </>
+    </Aux>
   );
 };
 
