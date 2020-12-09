e244a15558a35a2becb62ee349c1d06fe2f33d19
Venus Chan <vchan446@gmail.com>
2020-12-09 11:32:34 +0800

    try again

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,18 @@
 // Update with your config settings.
 // const path = require('path')
-require('dotenv').config()
+require('dotenv').config({ path: __dirname + '/.env' })
 
-// const devConfig = `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const devConfig = `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
-// const proConfig = process.env.DATABASE_URL; //heroku addons
+const proConfig = process.env.DATABASE_URL; //heroku addons
+
+const client = 'pg'
 
 
 module.exports = {
   development: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
+    client: client,
+    connection: process.env.NODE_ENV === "production" ? proConfig : devConfig,
     pool: {
       min: 2,
       max: 10
