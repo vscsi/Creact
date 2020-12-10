0d07d3c7fa154fd8639822f145c1b026f5c4e97e
wongw859 <wongw859@gmail.com>
2020-12-10 17:19:10 +0800

    submit again

--- server/controllers/register.js
+++ server/controllers/register.js
@@ -10,23 +10,22 @@ exports.postRegister= async(req,res)=>{
     let hashedPassword = await bcrypt.hash(password, 10);
     try{
       query = await knex.from('users').select().where('username',username);
+      if(query.length>0){
+        // console.log('from server/register.js__username is already registered, please pick a new one')
+        res.json({userNameRepeated: true})
+      }else{
+        await knex('users')
+          .insert({
+            username: username,
+            user_pw: hashedPassword,  
+            first_name: firstname,
+            last_name: lastname,
+            email: email
+          })
+          res.json({userNameRepeated:false})
+      }
     }catch(e){
       console.error(e.message)
     }
-    // console.log('from server/register.js__',query, query.length)
-
-    if(query.length>0){
-      console.log('from server/register.js__username is already registered, please pick a new one')
-    }else{
-      await knex('users')
-        .insert({
-          username: username,
-          user_pw: hashedPassword,  
-          first_name: firstname,
-          last_name: lastname,
-          email: email
-        })
-    }
-  
-  // console.log(username, password)
 }
+
