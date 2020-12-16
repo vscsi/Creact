3815ac3976ae04971b580b37441610ef58636cc9
Venus Chan <vchan446@gmail.com>
2020-12-16 11:55:57 +0800

    added migration

--- server/migrations/20201130133733_chatroom_table.js
+++ server/migrations/20201130133733_chatroom_table.js
@@ -2,10 +2,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('chatroom', (table)=>{
         table.increments('id').primary();
-        table.boolean('chatroom_type').defaultTo('false').notNullable;
-        table.integer('number_of_users');
-        table.integer('workspace_id').unsigned();
-        table.foreign('workspace_id').references('workspace.id');
+        table.boolean('chatroom_type').defaultTo('true').notNullable;
     })
   };
   
