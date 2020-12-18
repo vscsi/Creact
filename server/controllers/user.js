e6f314caf81e57d17b7000fae6c8d467dae7d067
oneheadking <50674351+oneheadking@users.noreply.github.com>
2020-12-18 11:42:14 +0800

    Check online users

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -18,3 +18,26 @@ exports.getUserName = async (req, res) => {
     console.error(error.message);
   }
 };
+
+exports.checkLoginUsers = async (req, res) => {
+  try {
+    //if user log in, will save the userName into loginusers
+    //if user log out, will remove the userName from loginusers
+    console.log(`in checkLoginUsers route`)
+    let loginUsers = [];
+    console.log(loginUsers);
+    //when user first login, in dashboardprofilecontinaer
+    if (req.body.userName === "") {
+      loginUsers.push({name: req.userName});
+    } else {
+      //have data post to this route
+      loginUsers = loginUsers.filter((item, index) => {
+        return item.name !== req.body.userName;
+      });
+    }
+    console.log(loginUsers);
+    res.json({ loginUsers: loginUsers });
+  } catch (error) {
+    console.error(error.message);
+  }
+};
