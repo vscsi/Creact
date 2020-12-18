b5ea3cf566ffdb01dc19ade921ad8c9c72fc4022
Venus Chan <vchan446@gmail.com>
2020-12-18 15:55:51 +0800

    nouse

--- client/src/Pages/DashboardPage/DashboardFeatures/CollaborationNotePage/CollabNoteContainer.js
+++ client/src/Pages/DashboardPage/DashboardFeatures/CollaborationNotePage/CollabNoteContainer.js
@@ -1,85 +1,25 @@
-import React, { Component } from 'react'
-import { EditorState }  from 'draft-js'
+
+import React, { useState } from 'react'
+import { 
+    EditorState, 
+    convertToRaw, 
+    convertFromRaw, 
+    Editor, 
+    RichUtils, 
+}  from 'draft-js'
 // import draftToHtml from 'draftjs-to-html;
 // import { Editor } from 'react-draft-wysiwyg'
 import './CollaborationNote.css'
-// import '../../../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import '../../../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+// import Axios from 'axios';
 
-import { CKEditor, CKEditorContext } from '@ckeditor/ckeditor5-react';
-import DecoupledDocumentEditor from '@ckeditor/ckeditor5-editor-decoupled/src/decouplededitor.js';
-import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
-import AutoImage from '@ckeditor/ckeditor5-image/src/autoimage.js';
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
-import Autolink from '@ckeditor/ckeditor5-link/src/autolink.js';
-import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave.js';
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
-import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock.js';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage.js';
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
-import ExportToPDF from '@ckeditor/ckeditor5-export-pdf/src/exportpdf.js';
-import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js';
-import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
-import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
-import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight.js';
-import Image from '@ckeditor/ckeditor5-image/src/image.js';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
-import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
-import Link from '@ckeditor/ckeditor5-link/src/link.js';
-import List from '@ckeditor/ckeditor5-list/src/list.js';
-import ListStyle from '@ckeditor/ckeditor5-list/src/liststyle.js';
-import PresenceList from '@ckeditor/ckeditor5-real-time-collaboration/src/presencelist.js';
-import RealTimeCollaborativeComments from '@ckeditor/ckeditor5-real-time-collaboration/src/realtimecollaborativecomments.js';
-import RealTimeCollaborativeEditing from '@ckeditor/ckeditor5-real-time-collaboration/src/realtimecollaborativeediting.js';
-import RealTimeCollaborativeTrackChanges from '@ckeditor/ckeditor5-real-time-collaboration/src/realtimecollaborativetrackchanges.js';
-import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
-import Table from '@ckeditor/ckeditor5-table/src/table.js';
-import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperties';
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
-import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
-import Title from '@ckeditor/ckeditor5-heading/src/title.js';
-import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
-import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount.js';
-
-class MyEditor extends Component {
-
-    render(){
-
-        return(
-            <div className="doc" >
-                <CKEditor 
-                    onError={ ( { willEditorRestart } ) => {
-                        // If the editor is restarted, the toolbar element will be created once again.
-                        // The `onReady` callback will be called again and the new toolbar will be added.
-                        // This is why you need to remove the older toolbar.
-                        if ( willEditorRestart ) {
-                            this.editor.ui.view.toolbar.element.remove();
-                        }
-                    } }
-                    onChange={ ( event, editor ) => console.log( { event, editor } ) }
-                    editor={ DecoupledDocumentEditor }
-                    data="<p>Hello from CKEditor 5's decoupled editor!</p>"
-                    config={ {
-                        plugins: [Alignment, AutoImage, Autoformat, Autolink, Autosave, BlockQuote, Bold, CodeBlock, EasyImage, Essentials, ExportToPDF, FontBackgroundColor, FontColor, FontSize, Heading, Highlight, Image, Indent, IndentBlock, Italic, Link, List, ListStyle, RealTimeCollaborativeComments, RealTimeCollaborativeEditing, PresenceList, RealTimeCollaborativeTrackChanges, Strikethrough, Table, TableCellProperties, TableToolbar, TextTransformation, Title, Underline, WordCount],
-                        toolbar: [ 'bold', 'italic', 'undo', 'redo']
-                    } }
-                    onReady={ editor => {
-                        console.log( 'Editor is ready to use!', editor );
-
-                        // Insert the toolbar before the editable area.
-                        editor.ui.getEditableElement().parentElement.insertBefore(
-                            editor.ui.view.toolbar.element,
-                            editor.ui.getEditableElement()
-                        );
-
-                        this.editor = editor;
-                    } }
-                    />
-            </div>
-        )
-    }
+function MyEditor() {
+    const [editorState, setEditorState] = React.useState(
+      () => EditorState.createEmpty(),
+    );
+  
+    return <Editor editorState={editorState} onChange={setEditorState} />;
 }
+    
 
 export default MyEditor
