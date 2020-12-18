6930fb71f51deb09353e794bbfe03975607759a6
Venus Chan <vchan446@gmail.com>
2020-12-18 16:51:55 +0800

    idk

--- client/src/Pages/DashboardPage/DashboardFeatures/CollaborationNotePage/CollabNoteContainer.js
+++ client/src/Pages/DashboardPage/DashboardFeatures/CollaborationNotePage/CollabNoteContainer.js
@@ -1,39 +1,32 @@
-import React, { Component } from 'react'
-import { EditorState }  from 'draft-js'
-// import draftToHtml from 'draftjs-to-html;
-import { Editor } from 'react-draft-wysiwyg'
-import './CollaborationNote.css'
-import '../../../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-
-class MyEditor extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            editorState: EditorState.createEmpty()
-        }
+import React, {useEffect} (() => {
+    effect
+    return () => {
+        cleanup
     }
-    
+}, [input])}  from 'react';
+import {
+    Editor, 
+    EditorState,
+    convertFromRaw,
+    convertToRaw
+} from 'draft-js';
+import 'draft-js/dist/Draft.css';
 
-    onEditorStateChange = editorState =>{
-        console.log('operation')
-        this.setState({
-            editorState
-        })
-    }
 
-    render(){
-        const { editorState } = this.state
+function MyEditor() {
+  const [editorState, setEditorState] = React.useState(
+    () => EditorState.createEmpty(),
+  );
+   useEffect(() => {
+       console.log(editorState)
+       return () => {
+           cleanup
+       }
+   }, [editorState])
+  
 
-        return(
-            <div className="doc" >
-                <Editor
-                    editorState={editorState}
-                    onEditorStateChange={this.onEditorStateChange}
-                    placeholder="The message goes here..." 
-                />
-            </div>
-        )
-    }
+  return <Editor editorState={editorState} onChange={setEditorState} />;
 }
+
 
 export default MyEditor
