735081de94a401a335a3c1641afd62b40ea94b51
Venus <vchan446@gmail.com>
2020-12-19 14:52:31 +0800

    added some style buttons

--- client/src/Pages/DashboardPage/DashboardFeatures/CollaborationNotePage/CollabNoteContainer.js
+++ client/src/Pages/DashboardPage/DashboardFeatures/CollaborationNotePage/CollabNoteContainer.js
@@ -1,31 +1,60 @@
-import React, {useEffect} (() => {
-    effect
-    return () => {
-        cleanup
-    }
-}, [input])}  from 'react';
+import React, {useCallback, useEffect} from 'react';
 import {
     Editor, 
     EditorState,
+    RichUtils,
     convertFromRaw,
     convertToRaw
 } from 'draft-js';
+
 import 'draft-js/dist/Draft.css';
 
 
 function MyEditor() {
-  const [editorState, setEditorState] = React.useState(
-    () => EditorState.createEmpty(),
-  );
-   useEffect(() => {
-       console.log(editorState)
-       return () => {
-           cleanup
-       }
-   }, [editorState])
-  
+    const [editorState, setEditorState] = React.useState(
+        () => EditorState.createEmpty(),
+    );
+    const handleKeyCommand = useCallback((command, editorState) => {
+        const newState = RichUtils.handleKeyCommand(editorState, command)
+        if(newState) {
+            setEditorState(newState)
 
-  return <Editor editorState={editorState} onChange={setEditorState} />;
+            return "handled"
+        }
+        return "not-handled"
+    })
+    const _onBoldClick = useCallback(() => {
+        setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"))
+    })
+
+    const _onItalicClick = useCallback(() => {
+        setEditorState(RichUtils.toggleInlineStyle(editorState, "ITALIC"))
+    })
+
+    const _onUnderlineClick = useCallback(() => {
+        setEditorState(RichUtils.toggleInlineStyle(editorState, "UNDERLINE"))
+    })
+
+    const _onCodeClick = useCallback(() => {
+        setEditorState(RichUtils.toggleInlineStyle(editorState, "CODE"))
+    })
+
+
+    return (
+        <>
+            <button onClick={_onBoldClick}>Bold</button>
+            <button onClick={_onItalicClick}>Italic</button>
+            <button onClick={_onUnderlineClick}>Underline</button>
+            <button onClick={_onCodeClick}>Code</button>
+
+
+
+            <Editor 
+                editorState={editorState}
+                handleKeyCommand={handleKeyCommand} 
+                onChange={setEditorState} />
+        </>
+    )
 }
 
 
