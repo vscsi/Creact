45b697e4bf4d718afb22d42d3236b52726fd40c4
Venus <vchan446@gmail.com>
2020-12-19 16:10:37 +0800

    added stuff

--- client/src/Pages/DashboardPage/DashboardFeatures/CollaborationNotePage/CollabNoteContainer.js
+++ client/src/Pages/DashboardPage/DashboardFeatures/CollaborationNotePage/CollabNoteContainer.js
@@ -1,4 +1,5 @@
 import React, {useCallback, useEffect} from 'react';
+// import classes from "./CollaborationNote.module.css"
 import {
     Editor, 
     EditorState,
@@ -6,9 +7,10 @@ import {
     convertFromRaw,
     convertToRaw
 } from 'draft-js';
-
 import 'draft-js/dist/Draft.css';
 
+import BlockStyleControls from './components/BlockStyles';
+import InlineStyleControls from './components/InlineStyles';
 
 function MyEditor() {
     const [editorState, setEditorState] = React.useState(
@@ -23,32 +25,49 @@ function MyEditor() {
         }
         return "not-handled"
     })
-    const _onBoldClick = useCallback(() => {
-        setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"))
-    })
+    // const _onBoldClick = useCallback(() => {
+    //     setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"))
+    // })
 
-    const _onItalicClick = useCallback(() => {
-        setEditorState(RichUtils.toggleInlineStyle(editorState, "ITALIC"))
-    })
+    // const _onItalicClick = useCallback(() => {
+    //     setEditorState(RichUtils.toggleInlineStyle(editorState, "ITALIC"))
+    // })
 
-    const _onUnderlineClick = useCallback(() => {
-        setEditorState(RichUtils.toggleInlineStyle(editorState, "UNDERLINE"))
-    })
+    // const _onUnderlineClick = useCallback(() => {
+    //     setEditorState(RichUtils.toggleInlineStyle(editorState, "UNDERLINE"))
+    // })
 
-    const _onCodeClick = useCallback(() => {
-        setEditorState(RichUtils.toggleInlineStyle(editorState, "CODE"))
-    })
+    // const _onCodeClick = useCallback(() => {
+    //     setEditorState(RichUtils.toggleInlineStyle(editorState, "CODE"))
+    // })
 
 
     return (
         <>
-            <button onClick={_onBoldClick}>Bold</button>
-            <button onClick={_onItalicClick}>Italic</button>
-            <button onClick={_onUnderlineClick}>Underline</button>
-            <button onClick={_onCodeClick}>Code</button>
-
-
-
+            {/* <button 
+                onClick={_onBoldClick}
+                className={classes.Bold}>
+                    Bold
+            </button>
+            <button 
+                onClick={_onItalicClick}
+                className={classes.Italic}>
+                    Italic
+            </button>
+            <button 
+                onClick={_onUnderlineClick}
+                className={classes.Underline}>
+                    Underline
+            </button>
+            <button 
+                onClick={_onCodeClick}
+                className={classes.Code}>
+                    Code
+            </button> */}
+            <BlockStyleControls
+                editorState={editorState}/>
+            <InlineStyleControls 
+                editorState={editorState}/>
             <Editor 
                 editorState={editorState}
                 handleKeyCommand={handleKeyCommand} 
