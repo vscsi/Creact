48eab3bd8c4f1b08425952f23b3e6ed6241574f7
Charles Choi <charlesc.inbox@gmail.com>
2020-12-21 18:58:17 +0800

    progress

--- client/src/Pages/LandingPage/LandingPageContainer.js
+++ client/src/Pages/LandingPage/LandingPageContainer.js
@@ -1,9 +1,10 @@
 import React from "react";
 import NavBar from "./Nav/NavBar";
 
+import {Link} from 'react-router-dom';
 import logo from './img/logo.png'
 
-import './landingpage.css';
+import './sass/landingpage.scss';
 
 
 function LandingPageContainer(props) {
@@ -16,17 +17,54 @@ function LandingPageContainer(props) {
           <img src={logo} alt="logo" className="logo"/>
         </div>
       
-      <div className="text-box">
-        <h1 className='heading-primary'>
+        <div className="text-box">
+          <h1 className='heading-primary'>
           <span className="heading-primary-main">Creact</span>
           <span className="heading-primary-sub">is where creativity happens</span>
         </h1>
-      </div>
+        <Link to="" className="btn btn-white btn-animated">Open Creact App</Link>
+        </div>
 
-        
       </div>
     )
   }
+
+  const Grid = () => {
+    return (
+      <div className="grid-test">
+        <div className="row">
+          <div className="col-1-of-2">Col 1 of 2</div>
+          <div className="col-1-of-2">Col 1 of 2</div>
+        </div>
+        <div className="row">
+          <div className="col-1-of-3">Col 1 of 3</div>
+          <div className="col-1-of-3">Col 1 of 3</div>
+          <div className="col-1-of-3">Col 1 of 3</div>
+        </div>
+        <div className="row">
+          <div className="col-1-of-3">Col 1 of 3</div>
+          <div className="col-2-of-3">Col 2 of 3</div>
+        </div>
+        <div className="row">
+          <div className="col-1-of-4">Col 1 of 4</div>
+          <div className="col-1-of-4">Col 1 of 4</div>
+          <div className="col-1-of-4">Col 1 of 4</div>
+          <div className="col-1-of-4">Col 1 of 4</div>
+        </div>
+        <div className="row">
+          <div className="col-1-of-4">Col 1 of 4</div>
+          <div className="col-1-of-4">Col 1 of 4</div>
+          <div className="col-2-of-4">Col 2 of 4</div>
+        </div>
+        <div className="row">
+          <div className="col-1-of-4">Col 1 of 4</div>
+          <div className="col-3-of-4">Col 3 of 4</div>
+
+        </div>
+      </div>
+    )
+  }
+
 
   const Card = (props) => {
     return (
@@ -52,6 +90,7 @@ function LandingPageContainer(props) {
       {/* <NavBar/> */}
       <div id = "body">
         <Header />
+        <Grid />
         <Card/>
         <Footer />
 
